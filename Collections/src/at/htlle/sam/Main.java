38564f62a7b7720d575153c29cff798cb14c79ce
SamuelWernig <samuelwernig@gmail.com>
2025-03-18 15:28:10 +0100

    Test

--- Collections/src/at/htlle/sam/Main.java
+++ Collections/src/at/htlle/sam/Main.java
@@ -1,5 +1,7 @@
+package at.htlle.sam;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World");
     }
 }
