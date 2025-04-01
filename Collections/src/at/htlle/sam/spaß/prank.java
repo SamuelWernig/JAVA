2bc935cabe81421199e3a156f5f5ac7b68aca9f4
SamuelWernig <samuelwernig@gmail.com>
2025-04-01 15:47:30 +0200

    carcollection Updated

---
+++ Collections/src/at/htlle/sam/spaß/prank.java
@@ -0,0 +1,11 @@
+package at.htlle.sam.spaß;
+
+import javax.swing.*;
+
+public class prank {
+    public static void main(String[] args) {
+        while (true) {
+            JOptionPane.showMessageDialog(null, "Fehler 404: Gehirn nicht gefunden! 😂", "Systemfehler", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
