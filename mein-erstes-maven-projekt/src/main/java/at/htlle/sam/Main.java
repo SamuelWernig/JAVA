b7323aa5b244dd5f013abbd15d00ed11c597ff1b
SamuelWernig <samuelwernig@gmail.com>
2025-04-10 14:15:49 +0200

    mein-erstes-maven-projekt updated

--- mein-erstes-maven-projekt/src/main/java/at/htlle/sam/Main.java
+++ mein-erstes-maven-projekt/src/main/java/at/htlle/sam/Main.java
@@ -1,8 +1,15 @@
 package at.htlle.sam;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import com.google.common.collect.Lists;  // Importiere die Lists-Klasse
 
+public class Main {
+    public static void main(String[] args){
+        // Erstelle eine neue ArrayList mit Guava
+        var list = Lists.newArrayList();
+        list.add("Samuel");
+        list.add("Jonas");
+        list.add("Bob");
+        list.add("Michael");
+        System.out.println(list);
     }
 }
--- mein-erstes-maven-projekt/src/main/java/at/htlle/sam/maven/HelloGuava.java
+++ mein-erstes-maven-projekt/src/main/java/at/htlle/sam/maven/HelloGuava.java
@@ -1,15 +1,23 @@
 package at.htlle.sam.maven;
 
-import com.google.common.collect.Lists;  // Importiere die Lists-Klasse
+import com.google.common.collect.Lists;
+import java.util.Comparator;
 
 public class HelloGuava {
-    public static void main(String[] args){
-        // Erstelle eine neue ArrayList mit Guava
-        var list = Lists.newArrayList();
-        list.add("Samuel");
-        list.add("Jonas");
-        list.add("Bob");
-        list.add("Michael");
+    public static void main(String[] args) {
+        // Erstelle eine neue ArrayList mit Guava und `var`
+        var list = Lists.newArrayList("Samuel", "Jonas", "Bob", "Michael");
+
+        // Ausgabe der unsortierten Liste
+        System.out.println("Unsortierte Liste:");
+        System.out.println(list);
+        System.out.println("----------------------------------------");
+
+        // Liste sortieren
+        list.sort(Comparator.naturalOrder());  // Sortiert die Liste in natürlicher Reihenfolge
+
+        // Ausgabe der sortierten Liste
+        System.out.println("Sortierte Liste:");
         System.out.println(list);
     }
 }
