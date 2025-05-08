e7620966a575bea900d32b12de5b76933457ecb7
SamuelWernig <samuelwernig@gmail.com>
2025-05-08 14:34:09 +0200

    Updated

--- Exceptions/src/at/htlle/sam/customException/CustomException.java
+++ Exceptions/src/at/htlle/sam/customException/CustomException.java
@@ -1,13 +1,28 @@
 package at.htlle.sam.customException;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CustomException {
-    public static void main(String[] args) {
-        String name = null;
-        try {
-            System.out.println(name.length());
-        }catch(RuntimeException e) {
+    public static void main(String[] args){
+        try{
+            readFile();
+        }catch(IOException e){
             e.printStackTrace();
         }
+    }
 
+    public static void readFile() throws IOException{
+        String fileName = "/Users/pankaj/source.txt";
+        File file = new File(fileName);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while((line = br.readLine()) != null){
+            //process the line
+            System.out.println(line);
+        }
     }
 }
