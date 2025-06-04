775e2556a33de70a7d2941f6339a2a878c5b397f
SamuelWernig <samuelwernig@gmail.com>
2025-06-04 09:05:45 +0200

    New Folder: MinecraftMod

---
+++ minecraftMod/src/client/java/at/htlle/sam/minecraftmod/client/MinecraftmodClient.java
@@ -0,0 +1,10 @@
+package at.htlle.sam.minecraftmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ minecraftMod/src/main/java/at/htlle/sam/minecraftmod/Minecraftmod.java
@@ -0,0 +1,10 @@
+package at.htlle.sam.minecraftmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Minecraftmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
