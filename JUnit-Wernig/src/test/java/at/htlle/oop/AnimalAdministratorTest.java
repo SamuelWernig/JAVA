84a60bf6ef8f17c7a5a008b494555b258a488475
SamuelWernig <samuelwernig@gmail.com>
2025-09-09 09:59:20 +0200

    Updated AnimalAdministratorTest

--- JUnit-Wernig/src/test/java/at/htlle/oop/AnimalAdministratorTest.java
+++ JUnit-Wernig/src/test/java/at/htlle/oop/AnimalAdministratorTest.java
@@ -1,10 +1,11 @@
 package at.htlle.oop;
 
-import at.htl.oop.Animal;
-import at.htl.oop.Cat;
-import at.htl.oop.Dog;
-import at.htl.oop.Zoo;
+import at.htl.oop.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,9 +13,44 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AnimalAdministratorTest {
 
     @Test
-    void testSound(){
+    void testSoundDog(){
         Animal animal = new Dog("Dog");
         assertEquals("Wuff!", animal.makeSound());
     }
+    @Test
+    void testSoundCat(){
+        Animal animal = new Cat("Cat");
+        assertEquals("Miau!", animal.makeSound());
+    }
+    @Test
+    void testAddAnimalandGetNumberOfAnimals(){
+        Zoo zoo = new Zoo();
+        zoo.addAnimal(new Dog("Dog"));
+        zoo.addAnimal(new Cat("Cat"));
+        assertEquals(2, zoo.getNumberOfAnimals());
+    }
+    @Test
+    void testGetAllSounds(){
+        Zoo zoo = new Zoo();
+        zoo.addAnimal(new Dog("Dog"));
+        assertEquals("Dog: Wuff!", zoo.getAllSounds());
+    }
+    @Test
+    void testMainOutput_containsAnimalSounds() {
 
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+
+        AnimalAdministration.main(new String[]{});
+
+
+        String output = out.toString();
+
+
+        assertTrue(output.contains("Yoda: Wuff"));
+        assertTrue(output.contains("Lea: Miau"));
+
+        System.setOut(System.out);
+    }
 }
