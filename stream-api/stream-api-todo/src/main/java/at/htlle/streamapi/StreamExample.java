f5fe227dcfe6a5a03a647d954c87d46d8d38a327
SamuelWernig <samuelwernig@gmail.com>
2025-09-18 13:41:08 +0200

    updated files

---
+++ stream-api/stream-api-todo/src/main/java/at/htlle/streamapi/StreamExample.java
@@ -0,0 +1,28 @@
+package at.htlle.streamapi;
+
+import java.util.*;
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        List<String> names = List.of("Anna", "Bernd", "Clara", "Daniel", "Eva");
+
+        // TODO: Gib alle Namen mit einer klassischen for-Schleife aus
+
+        // TODO: Gib alle Namen mit forEach auf der Collection aus
+
+        // TODO: Gib alle Namen mit Stream.forEach aus
+
+        // TODO: Filtere alle Namen mit mehr als 4 Zeichen (klassisch)
+
+        // TODO: Filtere alle Namen mit mehr als 4 Zeichen (Stream)
+
+        // TODO: Wandle alle Namen in Großbuchstaben um (klassisch)
+
+        // TODO: Wandle alle Namen in Großbuchstaben um (Stream)
+
+        // TODO: Gruppiere die Namen nach Länge (klassisch)
+
+        // TODO: Gruppiere die Namen nach Länge (Stream mit Collectors.groupingBy)
+    }
+}
