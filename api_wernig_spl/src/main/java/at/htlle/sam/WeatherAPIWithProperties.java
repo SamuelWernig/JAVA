4c6bcee82991894d6d3ed01c3f2e06bfed194d46
SamuelWernig <samuelwernig@gmail.com>
2025-09-23 15:10:02 +0200

    files updated

--- api_wernig_spl/src/main/java/at/htlle/sam/WeatherAPIWithProperties.java
+++ api_wernig_spl/src/main/java/at/htlle/sam/WeatherAPIWithProperties.java
@@ -7,8 +7,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Properties;
-import java.io.IOException;
-import java.io.InputStream;
 import org.json.JSONObject;
 
 public class WeatherAPIWithProperties {
